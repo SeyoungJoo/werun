3ef5770c812522e9368ac648d5fd85619fbcb499
Juyeon Kim <jay.jykim91@gmail.com>
2020-12-01 17:31:29 +0900

    move dashboard to profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
-  def dashboard
-    @user = current_user
-    @received_requests = current_user.received_requests
-    @sent_requests = current_user.sent_requests
-  end
-  
   def home
   end
 end
