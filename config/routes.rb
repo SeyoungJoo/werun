dc8b5ee1b3470c1ea369469d24b3564fd2ae66ee
seyoungjoo <sellyjphoto@gmail.com>
2020-12-01 14:36:19 +0900

    created users controller and view, runners controller and the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,32 @@
 Rails.application.routes.draw do
-  get 'events/new'
-  get 'events/create'
-  get 'messages/new'
-  get 'messages/create'
-  get 'requests/new'
-  get 'requests/create'
-  get 'requests/update'
-  get 'tracks/index'
-  get 'tracks/show'
-  get 'tracks/new'
-  get 'tracks/create'
-  get 'runners/index'
-  get 'runners/show'
-  get 'runners/edit'
-  get 'runners/update'
+  # get 'users/edit'
+  # get 'users/update'
+  # get 'events/new'
+  # get 'events/create'
+  # get 'messages/new'
+  # get 'messages/create'
+  # get 'requests/new'
+  # get 'requests/create'
+  # get 'requests/update'
+  # get 'tracks/index'
+  # get 'tracks/show'
+  # get 'tracks/new'
+  # get 'tracks/create'
+  # get 'runners/index'
+  # get 'runners/show'
+  # get 'runners/edit'
+  # get 'runners/update'
+
   devise_for :users
   root to: 'pages#home'
+  resources :pages, only: [:index, :show]
+  resources :runners, only: [:index, :show]
+  resources :request, only: [:new, :create, :destroy]
+  resources :messages, only: [:new, :create]
+  resources :events, only: [:new, :create]
+  resource :profile, only: [:edit, :update, :show]
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+runner1 = User.create(
+  email: "jj@gmail.com",
+  password: "lewagon",
+  first_name: "John",
+  last_name: "Smith",
+  address: "Gangnam-gu"
+)
+
+runner2 = User.create(
+  email: "sarah@gmail.com",
+  password: "lewagon",
+  first_name: "Sarah",
+  last_name: "Kim",
+  address: "Dongdaemun-gu",
+)
