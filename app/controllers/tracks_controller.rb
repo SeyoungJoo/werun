f0da31c646ff4acc13dd3be44d0f2725788da542
HoJoong Kim <woid1221@gmail.com>
2020-12-04 12:41:14 +0900

    tracks controller proto

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,13 +1,34 @@
 class TracksController < ApplicationController
   def index
+    @tracks = policy_scope(Track).order(created_at: :desc)
   end
 
   def show
+    @track = Track.find(params[:id])
+    authorize @track
   end
 
   def new
+    @track = Track.new
+
+    authorize @track
   end
 
   def create
+    @track = Track.new(track_params)
+
+    if @track.save
+      # redirect_to track_path
+    else
+      render :new
+    end
+
+    authorize @track
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:name, :address, :description, :keyword, :start_lng, :end_lng, :start_lat, :end_lat)
   end
 end
