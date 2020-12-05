46bb59ad9bb3e061fe80d6b5be3ef5b8242dbf21
seyoungjoo <sellyjphoto@gmail.com>
2020-12-05 19:21:00 +0900

    calendar grid fixed

--- app/controllers/runners_controller.rb
+++ app/controllers/runners_controller.rb
@@ -1,26 +1,23 @@
 class RunnersController < ApplicationController
   def index
     @runners = policy_scope(User).order(created_at: :desc)
-
-    if params[:query].present?
-      @runners = User.search_by_address(params[:query])
-    else
-      @runners = policy_scope(User).order(created_at: :desc)
-    end
+      if params[:query].present?
+        @runners = User.search_by_address(params[:query])
+      else
+        @runners = policy_scope(User).order(created_at: :desc)
+      end
 
     @markers = @runners.geocoded.map do |runner|
-        if runner == current_user    
-        {
+        if runner == current_user
+          {
           lat: runner.latitude,
           lng: runner.longitude,
-          infoWindow: render_to_string(partial: "shared/info_window", locals: { runner: runner }),
           image_url:helpers.asset_url('my-location.JPG')
         }
       else
         {
           lat: runner.latitude,
           lng: runner.longitude,
-          infoWindow: render_to_string(partial: "shared/info_window", locals: { runner: runner }),
           image_url: Cloudinary::Utils.cloudinary_url(runner.image.key)
         }
       end
