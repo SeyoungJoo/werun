c22c203dd581158a762c84c2d2cc545972dca5cc
seyoungjoo <sellyjphoto@gmail.com>
2020-12-05 23:58:29 +0900

    map js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,17 +21,18 @@ import "bootstrap";
 import { initConversationCable } from '../channels/conversation_channel';
 import { initMapbox } from '../plugins/map';
 import "../plugins/flatpickr"
-import flatpickr from "flatpickr";
+// import flatpickr from "flatpickr";
 import { initFlatpickr } from '../plugins/flatpickr'
 // import { initSweetalert } from '../plugins/init_sweetalert';
 import { initSelect2 } from '../plugins/init_select2';
-
+// import { runner_card } from '../plugins/runner_card';
 
 document.addEventListener('turbolinks:load', () => {
   initFlatpickr();
   initConversationCable();
   initMapbox();
   initSelect2();
+  // runner_card();
   // initSweetalert('.buddy-request-button', {
   //   title: "Do you want to send a buddy request?",
   // });
