71d2b256c050f17798a3b5eb3d85499787affa35
seyoungjoo <sellyjphoto@gmail.com>
2020-12-06 20:55:19 +0900

    event page design

--- app/javascript/plugins/map.js
+++ app/javascript/plugins/map.js
@@ -7,7 +7,7 @@ const initMapbox = () => {
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 50, maxZoom: 10, duration: 2000 });
+    map.fitBounds(bounds, { padding: 70, maxZoom: 10, duration: 2000 });
   };
 
   const addMarkersToMap = (map, markers) => {
