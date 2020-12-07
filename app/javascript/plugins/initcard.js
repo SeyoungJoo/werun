6ef3982a0cdb4e7ddd9b132765d62af67f38308d
seyoungjoo <sellyjphoto@gmail.com>
2020-12-07 10:55:14 +0900

    added popup

--- app/javascript/plugins/initcard.js
+++ app/javascript/plugins/initcard.js
@@ -1,29 +1,22 @@
 const init_popup = () => { 
   // Get the modal
-  const modal = document.getElementById("popup-card");
-
-  // Get the button that opens the modal
-  const btn = document.getElementById("runner-click");
-
-  // Get the <span> element that closes the modal
-  const close = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on the button, open the modal
-  btn.onclick = function() {
-    modal.style.display = "block";
-  };
-
-  // When the user clicks on <span> (x), close the modal
-  close.onclick = function() {
-    modal.style.display = "none";
-  };
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+  const modals = document.querySelectorAll(".popup-card");
+  modals.forEach((modal) => {
+    const runnerid = modal.dataset.runnerId;
+    const btn = document.getElementById(`runner-click-${runnerid}`);
+    if (btn) {
+      btn.onclick = function() {
+        modal.style.display = "block";
+      };
     };
-  };
+
+    const close_popup = modal.querySelector(".close");
+    if (close_popup) { 
+        close_popup.onclick = function() {
+        modal.style.display = "none";
+      };
+    };
+  });
 };
 
 export { init_popup };
