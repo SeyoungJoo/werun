025bed0c4cf58daa9518185b4d0b9648553bfa41
seyoungjoo <sellyjphoto@gmail.com>
2021-01-20 14:03:32 +0900

    added SSL

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # SSL
+  config.force_ssl = true
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
